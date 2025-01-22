e28b5dd4c2ff9126a5e37c90d7acc06cb0efb0d7
MintBloom <a.stegmann09@gmail.com>
2025-01-22 00:09:48 +0000

    updated List-editing.py
    Code is still a work in progress.

--- List-editing.py
+++ List-editing.py
@@ -16,7 +16,11 @@ def Pick_a_list(dict_):    # parameter is a dictionary
         edit_choice = input('\n--> ')
         print(f'This is the current state of {edit_choice}:\n{dict_.get(edit_choice)}') # shows the list that the user has chosen to edit
         dict_.update({edit_choice:edit_list_menu(edit_choice, dict_)}) # will update the dictionary with the new list
-        print (dict_)
+        print (dict_)   # CURRENT CODE ENDS HERE
+
+
+
+
 
 def retrievedata() -> dict:
         try:
@@ -33,6 +37,10 @@ def dumpdata(DataToDump) -> None:
         f.write(DataToDump)
         f.close()
 
+
+
+
+
 def edit_list_menu (name, dict):
         # enables loop which will allow for continuous editing of a list
         list = valueFromDict(name, dict)  # returns only the list from the dictionary
@@ -46,7 +54,6 @@ def edit_list_menu (name, dict):
                         break
         return list # returns the new value of the list
                 
-
 def edit_list_menu_loop(list): # includes option which allows for a list to be edited times
         print ('What would you like to do? (Pick a number)')
         print ('- (1)Add something\n- (2)Add something in a particular position\n- (3)Delete somthing\n- (4)Quit editing this list')
@@ -61,6 +68,8 @@ def edit_list_menu_loop(list): # includes option which allows for a list to be e
                 case 3:
                         list = del_from_list(list)
                         return list # returns new and updated list
+
+
 
 
         
@@ -86,6 +95,8 @@ def del_from_list(list):
 
 
 
+
+
 def cycleThruKeys(dict):
         # will return all the keys in the dictionary
         for i in dict.keys():
@@ -106,19 +117,19 @@ def keyFromDict(name) -> str:
 
 def UserListCreate() -> dict:
         print('A list will be created; give a name.')
-        dictionary = dictionarymake(ListName(), ListProduct())  # the dictionary is created and at this point contains one name&list entry
+        dictionary = dictionarymake(ListName(), ListMake())  # the dictionary is created and at this point contains one name&list entry
         while True:                     # loop allows for names&lists to be continually created and added to the dictionary
                 print('Add another list?')
                 choice = input('--> ')
                 if choice in ('yes', 'Yes'):
-                        dictionary.update({ListName() : ListProduct()}) # name&list entry is created and added to the dictionary
+                        dictionary.update({ListName() : ListMake()}) # name&list entry is created and added to the dictionary
                         True
                 else:
                         break           # loop breaks here if user does not respond with yes
         return dictionary
 
 def ListName() -> str:
-        # create a name which will be associated with a list from ListProduct()
+        # create a name which will be associated with a list from ListMake()
         print('What would you like the list name to be?')
         new_name = str(input('--> '))
         return new_name
@@ -126,7 +137,8 @@ def ListName() -> str:
 
 
 
-def ListProduct() -> list:
+
+def ListMake() -> list:
         # create a list which will be associated with a name from ListName()
         List = []
         return List
@@ -136,14 +148,6 @@ def dictionarymake(name, list) -> dict: # parameters 'name' will be used a key a
         list_names = {}  # dictionary created
         list_names[name] = list   # adds entry to the dictionary with the corresponding function parameters
         return list_names
-
-''' To do lists:
-add things
-remove things
-rename a list
-add more lists
-delete lists
-'''
 
 if __name__ == '__main__':
         main()
